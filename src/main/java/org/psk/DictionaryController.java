00a72b947f728d7f013107eb00382cf9e30b16fa
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2023-06-04 16:28:14 +0200

    Added simplified edition of items (words) from hintListView

--- src/main/java/org/psk/DictionaryController.java
+++ src/main/java/org/psk/DictionaryController.java
@@ -1,13 +1,21 @@
 package org.psk;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
+import java.util.EventListener;
 import java.util.Queue;
 
 public class DictionaryController {
@@ -22,10 +30,23 @@ public class DictionaryController {
 
     @FXML
     private TextField searchTextField;
+    private long previousClickTime = 0;
+    private String previousClickedHintWord = "";
 
     public void initialize() {
         model = new DictionaryModel("tmp.txt");
+        hintListView.setEditable(true);
         hintListView.setOnMouseClicked(this::handleHintListViewClick);
+        hintListView.setCellFactory(TextFieldListCell.forListView());
+
+        hintListView.setOnEditCommit(new EventHandler<ListView.EditEvent<String>>() {
+            @Override
+            public void handle(ListView.EditEvent<String> t) {
+                hintListView.getItems().set(t.getIndex(), t.getNewValue());
+                System.out.println("setOnEditCommit");
+            }
+        });
+
     }
 
 
@@ -61,13 +82,29 @@ public class DictionaryController {
     }
 
     private void handleHintListViewClick(MouseEvent event) {
-        if (event.getClickCount() == 1) {
+        if (event.getClickCount() == 1) { // Check for single click
             String clickedHintWord = hintListView.getSelectionModel().getSelectedItem();
+            if(clickedHintWord == null) return;
+            // Calculate the time difference since the previous click
+            long currentClickTime = System.currentTimeMillis();
+            long clickTimeDifference = currentClickTime - previousClickTime;
+
+            if (clickedHintWord.equals(previousClickedHintWord) && clickTimeDifference <= 1000) {
+                // Same item clicked twice within a 1-second gap
+//                model.editWord(clickedHintWord, "A");
+                System.out.println("editing");
+            }
+
+            // Update the previous click information
+            previousClickTime = currentClickTime;
+            previousClickedHintWord = clickedHintWord;
+
             searchTextField.setText(clickedHintWord);
             updateHintListView(clickedHintWord);
             updateTranslationsListView(clickedHintWord);
         }
     }
+
 
     @FXML
     public void switchLanguage() {
